ebb7d097bfd56c3a0087dda745b22583f5b41fad
Nico <napalumbo@gmail.com>
2024-02-17 02:45:23 +0100

    tidyup receiver name on secretsreader

--- internal/secretsreader/secretsreader.go
+++ internal/secretsreader/secretsreader.go
@@ -32,34 +32,34 @@ type SecretReader interface {
 	DeleteSecretEntry(secretEntry secretsdb.SecretEntry) bool
 }
 
-func (ckdb DefaultSecretsReader) ReadEntriesFromContentGroupedByPath() error {
-	secretsDB, err := secretsdb.ReadSecretsDBFromDBBytes(ckdb.ContentInBytes, ckdb.Password)
+func (dsr DefaultSecretsReader) ReadEntriesFromContentGroupedByPath() error {
+	secretsDB, err := secretsdb.ReadSecretsDBFromDBBytes(dsr.ContentInBytes, dsr.Password)
 	if err == nil {
 		loadedDB = &secretsDB
 	}
 	return err
 }
 
-func (ckdb DefaultSecretsReader) GetUriID() string {
-	return ckdb.UriID
+func (dsr DefaultSecretsReader) GetUriID() string {
+	return dsr.UriID
 }
 
-func (ckdb DefaultSecretsReader) GetFirstPath() string {
+func (dsr DefaultSecretsReader) GetFirstPath() string {
 	return loadedDB.PathsInOrder[0]
 }
 
-func (ckdb DefaultSecretsReader) GetEntriesForPath(path string) []secretsdb.SecretEntry {
+func (dsr DefaultSecretsReader) GetEntriesForPath(path string) []secretsdb.SecretEntry {
 	return loadedDB.EntriesByPath[path]
 }
 
-func (ckdb DefaultSecretsReader) WriteDBBytes() ([]byte, error) {
-	return loadedDB.WriteDBBytes(ckdb.Password)
+func (dsr DefaultSecretsReader) WriteDBBytes() ([]byte, error) {
+	return loadedDB.WriteDBBytes(dsr.Password)
 }
 
-func (ckdb DefaultSecretsReader) AddSecretEntry(secretEntry secretsdb.SecretEntry) {
+func (dsr DefaultSecretsReader) AddSecretEntry(secretEntry secretsdb.SecretEntry) {
 	loadedDB.AddSecretEntry(secretEntry)
 }
 
-func (ckdb DefaultSecretsReader) DeleteSecretEntry(secretEntry secretsdb.SecretEntry) bool {
+func (dsr DefaultSecretsReader) DeleteSecretEntry(secretEntry secretsdb.SecretEntry) bool {
 	return loadedDB.DeleteSecretEntry(secretEntry)
 }
