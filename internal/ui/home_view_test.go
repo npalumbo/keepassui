31daeae42a2efe02221cd3ac8da4c68b842def91
Nico <napalumbo@gmail.com>
2024-09-07 23:58:01 +0200

    Actually test that translations work

--- internal/ui/home_view_test.go
+++ internal/ui/home_view_test.go
@@ -1,16 +1,21 @@
 package ui_test
 
 import (
+	"embed"
 	mocks_ui "keepassui/internal/mocks/ui"
 	"keepassui/internal/ui"
 	"testing"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/lang"
 	"fyne.io/fyne/v2/test"
 	"fyne.io/fyne/v2/widget"
 	"go.uber.org/mock/gomock"
 )
+
+//go:embed testdata/files/translation
+var translations embed.FS
 
 func TestCreateHomeView(t *testing.T) {
 	w := test.NewWindow(container.NewWithoutLayout())
@@ -22,6 +27,36 @@ func TestCreateHomeView(t *testing.T) {
 	w.Resize(fyne.Size{Width: 600, Height: 600})
 
 	test.AssertImageMatches(t, "CreateHomeView.png", w.Canvas().Capture())
+}
+
+func TestCreateHomeViewWithLangSpanish(t *testing.T) {
+	t.Setenv("LANG", "es")
+	err := lang.AddTranslationsFS(
+		translations, "testdata/files/translation",
+	)
+	if err != nil {
+		t.Error("Error initialising translations")
+	}
+
+	t.Cleanup(func() {
+		t.Setenv("LANG", "en")
+		err = lang.AddTranslationsFS(
+			translations, "testdata/files/translation",
+		)
+		if err != nil {
+			t.Error("Error initialising translations")
+		}
+	})
+
+	w := test.NewWindow(container.NewWithoutLayout())
+
+	dbFileEntry := ui.CreateDBFileEntry(nil, w)
+	homeView := ui.CreateHomeView(&dbFileEntry, nil, w)
+
+	w.SetContent(homeView.GetPaintedContainer())
+	w.Resize(fyne.Size{Width: 600, Height: 600})
+
+	test.AssertImageMatches(t, "CreateHomeView_Spanish.png", w.Canvas().Capture())
 }
 
 func TestHomeViewTapOnNewDBCallsTakeOverPasswordConfirm(t *testing.T) {
